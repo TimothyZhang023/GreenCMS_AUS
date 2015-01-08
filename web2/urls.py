41b2eba4223f4672d7e2230efedc177ce09d67b0
zts1993 <zts1993@gmail.com>
2015-01-08 23:33:55 +0800

    small improvement 20150108

--- web2/urls.py
+++ web2/urls.py
@@ -3,9 +3,11 @@ from web2 import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 
+
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns('web2.views',
+                       url(r'^$', 'index', name='manage_index'),
                        url(r'^api/', include('api.urls')),
                        url(r'^manage/', include('home.urls')),
                        url(r'^auth/', include('auth.urls')),
