99a6e5261ce4d53a8ee0ef86ea62a2e342283300
zts1993 <zts1993@gmail.com>
2015-10-05 16:24:32 +0800

    clearall

--- auth/urls.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'TianShuo'
-from django.conf.urls import patterns, url
-
-urlpatterns = patterns('auth.views',
-                       url(r'^login/$', 'auth_login', name='auth_login'),
-                       url(r'^logout/$', 'auth_logout', name='auth_logout'),
-
-)
--- web2/views.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'TianShuo'
-from django.http import HttpResponseRedirect
-from django.core.urlresolvers import reverse
-
-
-def index(request):
-    return HttpResponseRedirect(reverse('manage_dashboard'))
-
